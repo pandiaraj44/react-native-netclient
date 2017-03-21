dbd0757dedc44f1c5e8e7c5da63e1bd2c41b44bc
Pandiarajan Nagarajan <pandiaraj44@gmail.com>
2017-03-21 17:45:27 +0530

    Get method added

--- index.ios.js
+++ index.ios.js
@@ -12,12 +12,37 @@ import {
   View
 } from 'react-native';
 
-export default class RNETWORK extends Component {
+import {
+  Request,
+  Response,
+  RNETWORK
+} from './main-index';
+
+export default class RNETWORKExample extends Component {
   render() {
+    let request = new Request("http://demo9062349.mockable.io/country");
+    request.setQueryParams({
+      code : 'US'
+    });
+    RNETWORK.get(request, () => {
+      // Pre execute call back
+      //This will be called before making API call
+      //Show the loader here
+    }, (response: Response) =>{
+      // Post execute call back
+      // Success and Failure resposne in same callback
+      //Check for error then process
+      if(response.getError()){
+
+      }else{
+        alert(response.getBodyString());
+      }
+
+    });
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Ios Network Library
+          Network Library Example
         </Text>
       </View>
     );
@@ -38,4 +63,4 @@ const styles = StyleSheet.create({
   },
 });
 
-AppRegistry.registerComponent('RNETWORK', () => RNETWORK);
+AppRegistry.registerComponent('RNETWORK', () => RNETWORKExample);
---
+++ main-index.js
@@ -0,0 +1,9 @@
+import Request from './lib/Request';
+import Response from './lib/Response';
+import RNETWORK from './lib/RNETWORK';
+
+module.exports = {
+  Request: Request,
+  Response: Response,
+  RNETWORK: RNETWORK
+}
